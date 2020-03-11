ff90fe96001ff21e1db3283abc6b9f1c11fab10b
Yung Cher Ho <ycho527@hotmail.com>
2020-03-11 14:39:02 +0800

    Completed rails yelp app. Styling to be completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'reviews/show'
+  get 'reviews/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create, :index]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Dropping database'
+
+Restaurant.destroy_all
+
+puts 'Seeding...'
+
+10.times do
+  categories = ["chinese", "italian", "japanese", "french", "belgian"]
+  Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address, category: categories.sample, phone_number: Faker::PhoneNumber.cell_phone)
+end
+
+puts 'Completed seeding'
